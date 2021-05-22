44beb9b087ae3651bea13581d6676ab2d0d7db68
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-05-22 14:34:40 +0700

    Add project files.

---
+++ SK-Lib-cmath-cos-Bagian4__CPP/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by Programiz
+	Modified for Learn by Rizky Khapidsyah
+	I.D.E : VS19
+*/
+
+using namespace std;
+
+int main(){
+	int x = 1;
+	double result;
+
+	// result is in double
+	result = cos(x);
+	cout << "cos(x) = " << result << endl;
+
+	_getch();
+	return 0;
+}
